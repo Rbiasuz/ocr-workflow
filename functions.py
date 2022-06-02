f15241c501d25c918c07929337592ac5e52d6906
Elias de Moura <moura.elias.moura@gmail.com>
2022-06-02 15:07:24 -0300

    02/06

--- functions.py
+++ functions.py
@@ -1,13 +1,22 @@
-import numpy as np
-import pytesseract
-import PIL
+
 
 def get_text(image):
     """ 
     Recebe uma imagem e retorna o texto presente na mesma (string)
     """
+    import numpy as np
+    import pytesseract
+    import PIL
+    import re
+    import string
+
     im = PIL.Image.open(image)##'4e486cf2-62e6-4e36-8d2c-dfbe6ae1f001.jpg')
     txt = pytesseract.image_to_string(im)
+    txt = re.sub(' +', ' ', txt)
+    txt = txt.replace('\n','')
+    txt = txt.lower()
+    txt = ' '.join([word for word in txt.split() if word not in string.punctuation])
+
 
     ##TODO
 
