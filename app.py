7ecef6a307fbff0c68b7a78e07960611b0a4b671
Elias de Moura <moura.elias.moura@gmail.com>
2022-06-08 19:32:42 -0300

    alteracoes para funcionar o template html

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ import os
 from flask import Flask, render_template, request
 from functions import *
 
+app = Flask(__name__,template_folder='templates')
 
-app = Flask(__name__)
-
-
+# routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
     return render_template("index.html")
-
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
     if request.method == 'POST':
         img = request.files['my_image']
-        p = get_text(img)
 
-    return render_template("index.html", prediction = p)
+        img_path = "/media/assets/base-images/" + img.filename
+        img.save(img_path)
+
+        p = get_text(img_path)
+
+    return render_template("index.html", prediction = p, img_path = img_path)
 
 
 if __name__ =='__main__':
-    port = 5000 #int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
