afd716ca5bc9c579ecfcb69a3a4975a00c1b78d3
Elias de Moura <moura.elias.moura@gmail.com>
2022-06-30 16:02:16 -0300

    aula

--- crawler.py
+++ crawler.py
@@ -11,6 +11,9 @@ mainfolder = config['crawler']['mainfolder'][0]
 backupfolder = config['crawler']['backupfolder'][0]
 fileextennsion = config['crawler']['type'][0]
 timedelay = config['crawler']['interval'][0]
+targets = list(config.keys())
+targets.remove('crawler')
+
 
 def stringToList(string):
     listRes = list(string.split(","))
@@ -30,12 +33,31 @@ def files(path):
 while True:
     time.sleep(int(timedelay))
     for file in files(mainfolder):
-        ##move arq
-        shutil.move(mainfolder+"/"+file, backupfolder+"/"+file)
+        achou = 0
+        try:
+            texto = get_text(mainfolder+"/"+file)
+            for target in targets:
+                keyword = config[target]['keywords']
+                formula = config[target]['formula']
+                threshold = config[target]['threshold']
+                foldersalvar = config[target]['folder']
 
+                for word in texto.split():
+                    for kwy in keyword:
+                        p = string_distance(word,key,formula)
 
+                        if p >= int(threshold):
+                            salva_imagem(mainfolder+"/"+file,foldersalvar,file):##move arq
+                            achou = 1
+                            break
+        except:
+            shutil.move(mainfolder+"/"+file, backupfolder+"/"+file)
+            print('Erro ao processar arquivo '+file)
 
-
+        if achou = 0:
+            shutil.move(mainfolder+"/"+file, backupfolder+"/"+file)
+        else:
+            os.remove(mainfolder+"/"+file)
 
 
 #while true para intervalo de tempo
